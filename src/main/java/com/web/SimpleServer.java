9c4031e197ea2c6d0cc570be0dea40fba84dee9b
1deme <demetrelabadze1@gmail.com>
2025-01-07 00:42:53 +0400

    add server and front

--- src/main/java/com/web/SimpleServer.java
+++ src/main/java/com/web/SimpleServer.java
@@ -12,7 +12,7 @@ public class SimpleServer {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/solve", new SolveHandler());
-        server.setExecutor(null);
+        server.setExecutor(null); // Default executor
         server.start();
         System.out.println("Server started on port 8080");
     }
@@ -20,16 +20,25 @@ public class SimpleServer {
     static class SolveHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws java.io.IOException {
-            if ("POST".equals(exchange.getRequestMethod())) {
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+
+            if ("POST".equalsIgnoreCase(exchange.getRequestMethod())) {
                 // Read input
                 String body = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
 
+                // Parse the inputs
+                String[] inputs = body.split("&");
+                String equation1 = inputs[0].split("=")[1];
+                String equation2 = inputs[1].split("=")[1];
+
+                // Concatenate the inputs
+                String result = "Concatenated Result: " + equation1 + " " + equation2;
 
                 // Send response
                 exchange.getResponseHeaders().set("Content-Type", "text/plain");
-                exchange.sendResponseHeaders(200, body.length());
+                exchange.sendResponseHeaders(200, result.length());
                 OutputStream os = exchange.getResponseBody();
-                os.write(body.getBytes(StandardCharsets.UTF_8));
+                os.write(result.getBytes(StandardCharsets.UTF_8));
                 os.close();
             } else {
                 exchange.sendResponseHeaders(405, -1); // Method Not Allowed
