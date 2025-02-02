da57f241d47ab3358bc8905de25a7b170b6da2b7
1deme <demetrelabadze1@gmail.com>
2025-02-03 00:08:37 +0400

    improved IU.

--- src/main/java/com/setUpMain.java
+++ src/main/java/com/setUpMain.java
@@ -11,11 +11,26 @@ import com.example.relations.Relation;
 import com.example.relations.relationCollection;
 
 public class setUpMain {
-    public static void setUpAbstractData(String equation, String cutValue, String relations){
+    public static void setUpAbstractData(String equation, String relations, String cutValue){
+        System.out.println("Equations:" + equation);
+        System.out.println("Relations: " + relations);
+        System.out.println("Cut Value: " + cutValue);
         SimilarityPredicate sim = UnificationEquationParser.parse(equation, Double.parseDouble(cutValue));
         List<Relation> relList = SimilarityRelationParser.getRelationList(relations);
         Sim.disjunction.add(new Conjunction(sim));
         relationCollection.add(relList);
+    }
+
+    public static void main(String[] args) {
+        String equation = "k_o(a) ~= b_o(x)";
+        String relations = "(m, o, 0.5)";
+        String cutValue = "0.5";
+        SimilarityPredicate sim = UnificationEquationParser.parse(equation, Double.parseDouble(cutValue));
+        List<Relation> relList = SimilarityRelationParser.getRelationList(relations);
+        Sim.disjunction.add(new Conjunction(sim));
+        relationCollection.add(relList);
+        Sim.solve();
+
     }
     
 }
